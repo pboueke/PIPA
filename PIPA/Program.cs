9abb64c49dbfc58f4a578d3603055d2f65d9f00f
Pedro Boueke <pedro.boueke@bigdatacorp.com.br>
2018-12-04 16:56:51 -0200

    Added startup routine

--- PIPA/Program.cs
+++ PIPA/Program.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
-
+using System.Diagnostics;
+using PIPA.Utils;
 
 namespace PIPA
 {
@@ -37,7 +38,115 @@ namespace PIPA
         {
             logger.Info("Start");
 
+            logger.Info("Reading configuration file...");
+            logger.Info("Initializing stages and buffers...");
+            config.Initialize();
+
+            using (CancellationManager cm = new CancellationManager())
+            {
+                List<Task> stages = new List<Task>();
+                TaskFactory factory = new TaskFactory(TaskCreationOptions.LongRunning, TaskContinuationOptions.None);
+
+                logger.Info("Initializing threads...");
+                foreach (StageConfiguration s in config.StageList)
+                {
+                    // select buffers
+                    StageBuffer input = config.BufferList.Select(x => x.BufferName).Contains(s.InputBufferName) ?
+                        config.BufferList.Where(x => x.BufferName.Equals(s.InputBufferName)).FirstOrDefault() : null;
+                    List<StageBuffer> output = (s.OutputBufferNames == null) ? null :
+                        config.BufferList.Where(x => s.OutputBufferNames.Contains(x.BufferName)).ToList();
+                    // initialize modules
+                    s.Stage.Initialize(s.StageParameters);
+                    // start threads
+                    int threadsNumber = s.Stage.AllowMultiThreading ? s.StageThreadsNumber : 1;
+                    for (int i = 0; i < threadsNumber; i++)
+                    {
+                        if (s.Stage.RequireCancellationToken) cm.IncrementStopRequirement();
+                        if (output != null) foreach (StageBuffer sb in output) sb.Consumers += 1;
+                        var thread = factory.StartNew(() => s.Stage.Run(
+                            (input == null) ? null : input.Buffer.GetConsumingEnumerable(),
+                            (output == null) ? null : output.Select(x => x.Buffer).ToList(),
+                            cm, logger));
+                        stages.Add(thread);
+                        s.Threads.Add(thread);
+                    }
+                }
+                if (cm.RequiredRequestsForStopping == 0) Utils.ConsoleUtils.ConfirmEndlessness(!config.ConfirmEndlesness);
+                logger.Info("Waiting for conclusion...");
+                Wait(stages.ToArray(), config.BufferList, cm);
+                logger.Info("Execution complete...");
+            }
+
             logger.Info("End");
+        }
+
+        private static void Wait(Task[] stages, List<StageBuffer> buffers, CancellationManager cm)
+        {
+            ConsoleKeyInfo cki;
+            Tabler t = new Tabler(buffers);
+            Stopwatch timer = null;
+            if (config.AutoCancellationTimeout > 0)
+            {
+                timer = new Stopwatch();
+                timer.Start();
+            }
+
+            while (true)
+            {
+                // Check if the user requested a cancellation
+                while (Console.KeyAvailable)
+                {
+                    cki = Console.ReadKey();
+                    if (cki.Key == ConsoleKey.X)
+                    {
+                        logger.Info("Abortion requested by user. Stopping.");
+                        cm.RequestStop(true);
+                    }
+                }
+
+                // Check if autocancellation should be issued
+                if (timer != null && timer.ElapsedMilliseconds > config.AutoCancellationTimeout)
+                {
+                    if (buffers.All(x => x.Buffer.Count == 0))
+                    {
+                        timer = null;
+                        logger.Info("Reached autocancellation timeout. Stopping.");
+                        cm.RequestStop();
+                    }
+                    else timer.Restart();
+                }
+
+                Guid.NewGuid().ToString().Substring(0, 12);
+
+                // If a cancellation was requested, we must flood the pipeline with empty objects to make sure
+                // that any stage thread blocked or starved is able to check the cancellation status.
+                if (cm.Stop())
+                {
+                    CancellationNotice cn = new CancellationNotice();
+                    foreach (var b in buffers)
+                    {
+                        for (int i = 0; i < b.Consumers * 2; i++)
+                            b.Buffer.TryAdd(cn);
+                    }
+                }
+
+                // Print status
+                if (config.EnableMonitoring)
+                {
+                    t.PrintInfo(config.MonitoringSkip);
+                    t.PrintStagesTable(config.StageList, config.MonitoringSkip);
+                    t.PrintBuffersTable(buffers, config.MonitoringSkip);
+                }
+
+                // If all is well, wait some time and check if the execution is completed
+                if (Task.WaitAll(stages.ToArray(), config.MonitoringDelay))
+                {
+                    // END
+                    if (config.EnableMonitoring)
+                        t.LogBuffersUsage(logger);
+                    break;
+                }
+            }
         }
     }
 }
